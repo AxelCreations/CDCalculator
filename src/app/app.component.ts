fa002fb6b78076e79d28ab01521cb7faa6352e6a
AxelCreations <reynelaxel@gmail.com>
2020-10-12 22:25:43 -0400

    Added form binding and validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CapitalizedRow } from './models/capitalized.model';
 import { CreditRow } from './models/credit.model';
 
@@ -7,6 +8,36 @@ import { CreditRow } from './models/credit.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'CDCalculator';
+  public showResultsSection = false;
+  form = new FormGroup({
+    months: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(12)
+    ]),
+    depositDate: new FormControl('', [
+      Validators.required
+    ]),
+    depositAmount: new FormControl('', [
+      Validators.required,
+      Validators.min(1)
+    ]),
+    depositRate: new FormControl('', [
+      Validators.required,
+      Validators.min(0.5)
+    ])
+  });
+
+  public capitalizedRow: CapitalizedRow[] = [];
+  public creditRow: CreditRow[] = [];
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.showResultsSection = true;
+    } else {
+      this.showResultsSection = false;
+    }
+  }
 }
